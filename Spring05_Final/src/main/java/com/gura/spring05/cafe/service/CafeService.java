a6c8f49570509513829ab533257c84579f99444c
baekunyong <turbo6040@gmail.com>
2020-08-14 13:03:35 +0900

    Spring05_Final - 카페 목록 보기 기능 구현함.

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeService.java
@@ -1,5 +1,8 @@
 package com.gura.spring05.cafe.service;
 
-public interface CafeService {
+import javax.servlet.http.HttpServletRequest;
 
+public interface CafeService {
+	//카페 목록 보기 요청 관련 메소드
+	public void getList(HttpServletRequest request);
 }
